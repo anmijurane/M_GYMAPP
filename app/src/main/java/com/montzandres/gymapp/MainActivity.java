658e15fda4dad271bd4bca349c5969356e2e9395
Andres Jurado <anmijurane@gmail.com>
2020-06-17 19:55:00 -0500

    init proyect - gymAPP

---
+++ app/src/main/java/com/montzandres/gymapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.montzandres.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
