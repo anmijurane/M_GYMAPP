6d3485b77b956ccf617cb4dc5eff07c80e962697
Andres Jurado <anmijurane@gmail.com>
2020-06-25 23:46:24 -0500

    Added the function to start the application

--- app/src/main/java/com/montzandres/gymapp/App_Start.java
+++ app/src/main/java/com/montzandres/gymapp/App_Start.java
@@ -2,7 +2,9 @@ package com.montzandres.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class App_Start extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class App_Start extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app__start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                 try {
+                     Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                     startActivity(intent);
+                 }catch (Exception ign){
+                     ign.printStackTrace();
+                 }
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
