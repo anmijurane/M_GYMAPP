f0a4b5fbe49f8ad4210513ca6bf4e183bfdcc7a4
Andres Jurado <anmijurane@gmail.com>
2020-06-29 20:47:48 -0500

    add interace activity meta

---
+++ app/src/main/java/com/montzandres/gymapp/meta_activity.java
@@ -0,0 +1,14 @@
+package com.montzandres.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class meta_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meta_activity);
+    }
+}
