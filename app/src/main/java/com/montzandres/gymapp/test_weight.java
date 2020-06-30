7d1c72dcc58525be75942c40fefd0dd588c9cfd0
Andres Jurado <anmijurane@gmail.com>
2020-06-29 23:01:46 -0500

    add activity testing weight

---
+++ app/src/main/java/com/montzandres/gymapp/test_weight.java
@@ -0,0 +1,14 @@
+package com.montzandres.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test_weight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_weight);
+    }
+}
