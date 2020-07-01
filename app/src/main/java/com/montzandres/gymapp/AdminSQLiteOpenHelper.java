c17adffcda211d2e08b9aa8250d3e6c8a049356e
Andres Jurado <anmijurane@gmail.com>
2020-07-01 15:02:03 -0500

    add register user

--- app/src/main/java/com/montzandres/gymapp/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/montzandres/gymapp/AdminSQLiteOpenHelper.java
@@ -1,4 +1,33 @@
 package com.montzandres.gymapp;
 
-public class AdminSQLiteOpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    private SQLiteDatabase db;
+    private int oldVersion;
+    private int newVersion;
+
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable
+            SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        this.db = db;
+        db.execSQL("create table User()");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        this.db = db;
+        this.oldVersion = oldVersion;
+        this.newVersion = newVersion;
+    }
+
 }
