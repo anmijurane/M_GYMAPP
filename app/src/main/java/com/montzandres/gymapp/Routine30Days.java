f4658fb799dca39a71b4f5dc772ad92274bb5261
Andres Jurado <anmijurane@gmail.com>
2020-07-01 11:38:25 -0500

    add .xml and .java

---
+++ app/src/main/java/com/montzandres/gymapp/Routine30Days.java
@@ -0,0 +1,14 @@
+package com.montzandres.gymapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Routine30Days extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routine30_days);
+    }
+}
