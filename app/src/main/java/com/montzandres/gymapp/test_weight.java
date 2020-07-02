d8e362d808e10f8ef9c2c45d7b86269ed6ad8240
Andres Jurado <anmijurane@gmail.com>
2020-07-02 01:30:27 -0500

    botton activity funcion

--- app/src/main/java/com/montzandres/gymapp/test_weight.java
+++ app/src/main/java/com/montzandres/gymapp/test_weight.java
@@ -2,13 +2,45 @@ package com.montzandres.gymapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class test_weight extends AppCompatActivity {
-
+    private EditText sex, weight, age, height, current, regular;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_weight);
+        sex = findViewById(R.id.etT_sex);
+        weight = findViewById(R.id.etT_Weight);
+        age = findViewById(R.id.etT_Height);
+        current = findViewById(R.id.etT_CuActivity);
+        regular = findViewById(R.id.etT_ReActivity);
+
     }
+
+    public void NextWindows(View v){
+        if (ValidET()){
+        startActivity(new Intent(getApplicationContext(), meta_activity.class));
+        }else{
+        Toast.makeText(this,"Fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean ValidET(){
+        if(sex.getText().toString().equals("") |
+        weight.getText().toString().equals("") |
+        age.getText().toString().equals("")    |
+        height.getText().toString().equals("") |
+        current.getText().toString().equals("")|
+        regular.getText().toString().equals("")){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
 }
